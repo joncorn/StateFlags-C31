047836587544e7b4d3478be780bed24e301a787a
jon <jdcorn@live.com>
2020-01-27 09:54:59 -0700

    Initial Commit

---
+++ StateFlagsOBJC31/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StateFlagsOBJC31
+//
+//  Created by Jon Corn on 1/27/20.
+//  Copyright © 2020 Jon Corn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ StateFlagsOBJC31/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  StateFlagsOBJC31
+//
+//  Created by Jon Corn on 1/27/20.
+//  Copyright © 2020 Jon Corn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
