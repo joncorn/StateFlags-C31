685c27238b74aa1ade16b7168878cd128e1ba291
jon <jdcorn@live.com>
2020-01-27 10:15:58 -0700

    state model

---
+++ StateFlagsOBJC31/Models/State.h
@@ -0,0 +1,26 @@
+//
+//  State.h
+//  StateFlagsOBJC31
+//
+//  Created by Jon Corn on 1/27/20.
+//  Copyright © 2020 Jon Corn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface State : NSObject
+
+// MARK: - Properties
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *abbreviation;
+
+// MARK: - Initializer
+- (instancetype) initWithName:(NSString *)name
+                 abbreviation:(NSString *)abbreviation;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ StateFlagsOBJC31/Models/State.m
@@ -0,0 +1,13 @@
+//
+//  State.m
+//  StateFlagsOBJC31
+//
+//  Created by Jon Corn on 1/27/20.
+//  Copyright © 2020 Jon Corn. All rights reserved.
+//
+
+#import "State.h"
+
+@implementation State
+
+@end
